4509783f5d31d48717c8e045e62f1d4d5d916346
a-INTeger <49005281+a-INTeger@users.noreply.github.com>
2026-03-06 18:02:59 +0000

    main.py formatting clearup

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from math import e as E
 
 
 # setup parser for passing terminal parameters
-parser = argparse.ArgumentParser(
-    prog = "Hangman",
-    description="A small game of hangman"
+parser = argparse.ArgumentParser(prog="Hangman", description="A small game of hangman")
+parser.add_argument(
+    "-f",
+    "--file",
+    help="File containing wordbank, by default will use the provided WORDBANK.txt",
 )
-parser.add_argument("-f", "--file", help="File containing wordbank, by default will use the provided WORDBANK.txt")
 args = parser.parse_args()
 
 # ALL DEFAULT VALUES GO HERE
@@ -16,80 +17,98 @@ args = parser.parse_args()
 DEFAULT_FILE = "WORDBANK.txt"
 
 # Hangman ASCII art
-HANGMANPICS = [r'''
+HANGMANPICS = [
+    r"""
        
        
       
       
       
       
-         ''',r'''
+         """,
+    r"""
   +---+
   |   |
       |
       |
       |
       |
-=========''', r'''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
+      |
+      |
+      |
+=========""",
+    r"""
+  +---+
+  |   |
+  O   |
   |   |
       |
       |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
  /|   |
       |
       |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
  /|\  |
       |
       |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
  /|\  |
  /    |
       |
-=========''', r'''
+=========""",
+    r"""
   +---+
   |   |
   O   |
  /|\  |
  / \  |
       |
-=========''']
+=========""",
+]
 
 # Line size for sperating barriers
 LINE_SIZE = 64
+
+
 def mainMenu():
-    '''Outputs the main menu and returns the users choice of value'''
-    print("-"*LINE_SIZE +"\nMAIN MENU\n" + "1. Play game\n2. View high Scores\n3. Quit\n" + "-" * LINE_SIZE)
+    """Outputs the main menu and returns the users choice of value"""
+    print(
+        "-" * LINE_SIZE
+        + "\nMAIN MENU\n"
+        + "1. Play game\n2. View high Scores\n3. Quit\n"
+        + "-" * LINE_SIZE
+    )
     flag = False
     # validate whether the choice is numeric
     while not flag:
         # try to catch a keyboard interrupt to exit gracefully
         try:
-            choice = input("What would you like to do? (Input number corresponding to main menu)\n>> ")
+            choice = input(
+                "What would you like to do? (Input number corresponding to main menu)\n>> "
+            )
         except KeyboardInterrupt:
             print("\nSee you next time!")
             sys.exit(0)
-        
-        # catch any non number errors 
+
+        # catch any non number errors
         try:
             choice = int(choice)
         except:
@@ -105,7 +124,7 @@ def mainMenu():
 
 
 def setupWordbank():
-    '''Imports word bank from the file specified'''
+    """Imports word bank from the file specified"""
     try:
         # check whether the file was specified....
         if args.file:
@@ -130,11 +149,12 @@ def setupWordbank():
 
 
 def obscureWord(word):
-    '''Obscures a word with asterisks'''
+    """Obscures a word with asterisks"""
     return "*" * len(word)
 
+
 def playerGuess(word, priorGuess):
-    '''Handles player guess inputs'''
+    """Handles player guess inputs"""
 
     # flag for input validation
     flag = False
@@ -151,26 +171,29 @@ def playerGuess(word, priorGuess):
             print("\nThat was not a letter or a word!\nPlease try again.\n")
         # check whether guess is not a letter or a word guess
         elif len(guess) != 1 and len(guess) != len(word):
-            print("\nThat was not a single letter nor a word guess\nPlease try again.\n")
+            print(
+                "\nThat was not a single letter nor a word guess\nPlease try again.\n"
+            )
         # check if the guess has already been guessed
         elif guess in priorGuess:
             print("\nYou've already guessed that letter/word!\nPlease try again.\n")
         else:
             # trip flag and progress
             flag = True
-    
+
     # add to prior guesses
     priorGuess.append(guess)
     return guess
 
+
 def updateWord(chosenWord, maskedWord, guess, incorrectGuesses, score):
-    '''Updates a given word when a guess has been made and returns the new masked word and score'''
+    """Updates a given word when a guess has been made and returns the new masked word and score"""
 
     # check whether guess is word
     if len(guess) != len(chosenWord):
         # check if guess is in the word
         if guess in chosenWord:
-            # count all indices with guess 
+            # count all indices with guess
             idxs = [i for i, v in enumerate(chosenWord) if v == guess]
             maskedWordList = list(maskedWord)
             #  updates the masked word
@@ -195,11 +218,12 @@ def updateWord(chosenWord, maskedWord, guess, incorrectGuesses, score):
             incorrectGuesses += 1
             return maskedWord, incorrectGuesses, score
 
+
 def playGame(wordbank):
-    '''Core game loop function'''
+    """Core game loop function"""
     # track total score
     score = 0
-    
+
     # try again flag to keep replaying
     tryAgain = True
 
@@ -207,21 +231,23 @@ def playGame(wordbank):
         # choose random number to select word from wordbank
         randomIndex = random.randint(0, len(wordbank) - 1)
         chosenWord = wordbank[randomIndex]
-        incorrectGuesses =  0
-        
+        incorrectGuesses = 0
+
         # setup game displays
         priorGuesses = []
         maskedWord = obscureWord(chosenWord)
         # failure conditions for guess loop
         while (maskedWord != chosenWord) and incorrectGuesses < 7:
             # print the current game stat for the user
-            print("-"*LINE_SIZE)
+            print("-" * LINE_SIZE)
             print(HANGMANPICS[incorrectGuesses])
-            print("WORD TO GUESS:",  maskedWord)
+            print("WORD TO GUESS:", maskedWord)
             print("Past Guesses:", ", ".join(priorGuesses))
-            print("-"*LINE_SIZE)
+            print("-" * LINE_SIZE)
             guess = playerGuess(maskedWord, priorGuesses)
-            maskedWord, incorrectGuesses, score = updateWord(chosenWord, maskedWord, guess, incorrectGuesses, score)
+            maskedWord, incorrectGuesses, score = updateWord(
+                chosenWord, maskedWord, guess, incorrectGuesses, score
+            )
 
         # once out of the core game loop
         if incorrectGuesses < 7:
@@ -239,7 +265,7 @@ def playGame(wordbank):
                 choice = input("(Y/y = yes, otherwise no)>> ")
             except KeyboardInterrupt:
                 print("See you next time!")
-            
+
             # set the try again flag to restart game
             if choice.lower() == "y":
                 tryAgain = True
@@ -253,11 +279,12 @@ def playGame(wordbank):
             print("Your final score is:", score)
             print()
             tryAgain = False
-        
+
     return score
 
+
 def saveScore(score):
-    '''Saves score to a results.txt file'''
+    """Saves score to a results.txt file"""
     # allow user to give their name to save it in the txt file
     print()
     print("Congrats you got a new score! Enter your name to save it")
@@ -273,13 +300,14 @@ def saveScore(score):
     else:
         with open("results.txt", "a") as f:
             f.write(username + "," + str(score) + "\n")
-    
+
     # let the user know that it has been saved successfully
     print("Result saved successfully!")
     print()
 
+
 def printScores():
-    '''Displays the top 3 scores to the user'''
+    """Displays the top 3 scores to the user"""
     # temp data var for later output
     data = []
 
@@ -305,17 +333,18 @@ def printScores():
         properScores.sort(key=lambda x: x[1], reverse=True)
         # print out the top 3 results or fewer whichever is less
         print()
-        print("-"*LINE_SIZE)
+        print("-" * LINE_SIZE)
         for i in range(3 if len(data) >= 3 else len(data)):
             print(f"{i+1}. {properScores[i][0]:<20} Score: {properScores[i][1]:>4}")
-        print("-"*LINE_SIZE)
+        print("-" * LINE_SIZE)
         print()
     else:
         # let the user know there are no scores detected
         print("-" * LINE_SIZE + "\n\nNO SCORES DETECTED\n\n" + "-" * LINE_SIZE)
 
+
 def main():
-    '''Entry point for the entire hangman game'''
+    """Entry point for the entire hangman game"""
     choice = mainMenu()
     # keep looping if we dont want to quit
     while choice != 3:
@@ -328,12 +357,13 @@ def main():
             saveScore(finalScore)
         elif choice == 2:
             printScores()
-        
+
         choice = mainMenu()
     else:
         # print clean exit message
         print("See you next time!")
         sys.exit(0)
+
 
 # run only main() on python execution
 if __name__ == "__main__":
